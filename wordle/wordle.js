4a78b7a7404bc8cdfca85420f585773211bccc50
MusaJawad <musa.jawad@ucalgary.ca>
2023-02-17 17:19:32 -0700

    FETCH WORKS

--- wordle/wordle.js
+++ wordle/wordle.js
@@ -5,115 +5,28 @@ window.onload = function(){
 }
 
 
-
-const dict = [
-    {
-        "word": "Pain",
-        "hint": "Attending any class other than ENSF 381 gives you ____"
-    },
-    {
-        "word": "Nerd",
-        "hint": "You may be considered one, if you like Star Trek"
-    },
-    {
-        "word": "Main",
-        "hint": "Name of the function that starts a program in Golang and Java"
-    },
-    {
-        "word": "Rick",
-        "hint": "Lord of all memes"
-    },
-    {
-        "word": "Data",
-        "hint": "A USS Enterprise officer"
-    },
-    {
-        "word": "Byte",
-        "hint": "8 bits"
-    },
-    {
-        "word": "HTML",
-        "hint": "A markup language"
-    },
-    {
-        "word": "Snow",
-        "hint": "Bastards' last name in Game of Thrones"
-    },
-    {
-        "word": "Borg",
-        "hint": "An alien group in the Star Trek Universe"
-    },
-    {
-        "word": "Yoda",
-        "hint": "A true master, he is"
-    },
-    {
-        "word": "Code",
-        "hint": "As a softwae engineer, we need to do this a lot"
-    },
-    {
-        "word": "Time",
-        "hint": "It flies when you're in ENSF 381"
-    },
-    {
-        "word": "Lame",
-        "hint": "Compared to ENSF 381, your other courses are pretty ___"
-    },
-    {
-        "word": "Xbox",
-        "hint": "Like PS5, but worse"
-    },
-    {
-        "word": "Bash",
-        "hint": "Default shell on Ubuntu"
-    },
-    {
-        "word": "curl",
-        "hint": "A command-line tool for fetching data from the Internet"
-    },
-    {
-        "word": "wget",
-        "hint": "A command-line tool for downloading files from the Internet"
-    },
-    {
-        "word": "tail",
-        "hint": "A command-line tool for viewing the last part of a file"
-    },
-    {
-        "word": "JSON",
-        "hint": "A popular data-interchange format"
-    },
-    {
-        "word": "Arya",
-        "hint": "A character on Game of Thrones"
-    },
-    {
-        "word": "Ross",
-        "hint": "Pivot! Pivot!"
-    }
-]
-
-
-
-
-
-
 function checkWord(i,correctWord){
+    
     const myTable = document.getElementById('myTable');
 
-    userWord = []
-    
-    for (cell = 0;cell<4;cell++){
-        userWord.push(myTable.rows[i].cells[cell].innerHTML);
-    }
 
-    let stringUser = "";
-    for (let p = 0; p < userWord.length; p++) {
-        stringUser += userWord[p];
-    }
+    stringUser = "";
+
+    for (let cell = 0; cell < myTable.rows.length; cell++) {
+    stringUser += myTable.rows[i].cells[cell].innerHTML}
+
+    
+    
+
+    
+
+    
 
     correctWord = correctWord.toLowerCase();
     stringUser = stringUser.toLowerCase();
+
+    alert(correctWord)
+    alert(stringUser)
 
     for (let cell = 0; cell < myTable.rows.length; cell++) {
 
@@ -137,19 +50,38 @@ function checkWord(i,correctWord){
             }
         }
     }
+
     if(stringUser == correctWord ){
         for (let cell = 0; cell < myTable.rows.length; cell++) {
             myTable.rows[i].cells[cell].style.backgroundColor = "green";
         }
+        
         return true;
     }
+    
+
     return false;    
 }
 
 
 
+async function fetchDictionary() {
+    const apiKey = "sw0Tr2othT1AyTQtNDUE06LqMckbTiKWaVYhuirv";
+    const endpoint = "https://api.masoudkf.com/v1/wordle";
 
-function main(){
+    const res = await fetch(endpoint, {
+        headers: {
+        "x-api-key": apiKey,
+        },
+    });
+    const data = await res.json();
+    return data.dictionary;
+}
+
+
+
+
+async function main(){
 
     const myTable = document.getElementById('myTable');
     var i = 0;
@@ -158,10 +90,23 @@ function main(){
     myTable.rows[0].cells[0].style.border ="2px solid red" ;
     let dark = false;
     let image = false
+    let correctWord;
+    let correctHint;
+    
+    const dic =  await fetchDictionary();
 
-    indexValue = Number.parseInt(Math.random() * (dict.length))
-    correctWord = dict[4].word.toLowerCase();
-    correctHint = dict[4].hint;
+    const dict = JSON.parse(JSON.stringify(dic));
+    
+    
+    let randomIndex = Math.floor(Math.random() * dict.length);
+
+    
+    correctWord = JSON.stringify(dict[randomIndex].word)
+    correctHint = JSON.stringify(dict[randomIndex].hint)
+
+    correctWord = correctWord.replace(/['"]/g, "");
+
+    alert(correctWord)
 
     
 
@@ -170,7 +115,7 @@ function main(){
         for (row = 0;row<4;row++){
             for (col = 0;col<4;col++){
                 myTable.rows[row].cells[col].innerHTML = ""
-                myTable.rows[row].cells[col].style.backgroundColor = "grey";
+                myTable.rows[row].cells[col].style.backgroundColor = "antiquewhite";
                 myTable.rows[row].cells[col].style.border ="0px solid red" ;
 
 
@@ -182,8 +127,12 @@ function main(){
         document.getElementById("hint").innerHTML = ""
 
         indexValue = Number.parseInt(Math.random() * (dict.length))
-        correctWord = dict[indexValue].word;
-        correctHint = dict[indexValue].hint;
+        
+        correctWord = JSON.stringify(dict[indexValue].word);
+        correctHint = JSON.stringify(dict[indexValue].hint);
+        correctWord = correctWord.replace(/['"]/g, "");
+
+
         myTable.rows[0].cells[0].style.border ="2px solid red" ;
 
         alert(correctWord)
@@ -243,7 +192,7 @@ function main(){
     document.addEventListener("keyup", (k) =>{
 
 
-    if (k.code === 'Enter'){
+    if (k.code === 'Enter' || k.code === 'Return' ){
         
         if (j == 4){
 
